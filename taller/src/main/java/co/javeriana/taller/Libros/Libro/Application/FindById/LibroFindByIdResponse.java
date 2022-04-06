c2448e0a96d81234da13c7e850bb2f65b9ab6ec3
SMFragale <smolanof@javeriana.edu.co>
2022-04-05 19:27:00 -0500

    Se agregó un cambio pequeño

--- taller/src/main/java/co/javeriana/taller/Libros/Libro/Application/FindById/LibroFindByIdResponse.java
+++ taller/src/main/java/co/javeriana/taller/Libros/Libro/Application/FindById/LibroFindByIdResponse.java
@@ -3,6 +3,8 @@ package co.javeriana.taller.Libros.Libro.Application.FindById;
 import co.javeriana.taller.Libros.Libro.Domain.Libro;
 
 import java.util.HashMap;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class LibroFindByIdResponse {
 
@@ -13,7 +15,12 @@ public class LibroFindByIdResponse {
     }
 
     public HashMap<String, Object> response() {
-        HashMap<String, Object> response = libro.data();
+        HashMap<String, Object> response = new HashMap<>() {{
+            put("descripcion", libro.data().get("libroDescripcion"));
+            put("precio", libro.data().get("libroPrecio"));
+            put("añoPublicacion", libro.data().get("libroPublicationDate"));
+            put("numPaginas", new Random().nextInt(15000));
+        }};
         return response;
     }
 
